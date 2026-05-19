f53cffa3562f877008c737e4f51292be680fbcd7
nahrains <hassanhadi19996@gmail.com>
2026-05-19 13:17:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
 
-const CACHE_NAME = 'nahrain-student-v2.1';
+const CACHE_NAME = 'nahrain-student-v2.4';
 const ASSETS = [
     './index.html',
     './manifest.json',
